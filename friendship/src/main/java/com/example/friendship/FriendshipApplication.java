3ce15133a4283f038c2d91b0fb311d3a9cf8da02
sungchulyun <proattacker@naver.com>
2022-07-19 16:23:39 +0900

    GDSC 프로젝트 개인 깃으로 옮기는 중

--- friendship/src/main/java/com/example/friendship/FriendshipApplication.java
+++ friendship/src/main/java/com/example/friendship/FriendshipApplication.java
@@ -3,10 +3,8 @@ package com.example.friendship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class FriendshipApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(FriendshipApplication.class, args);
